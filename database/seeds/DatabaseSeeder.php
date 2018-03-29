f21b0e3ccedbd120775afc8c123c432c486e7933
Bojan Bajic <boki.deckonja@gmail.com>
2018-03-29 21:03:16 +0200

    Added images to seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -42,11 +42,14 @@ class DatabaseSeeder extends Seeder
             $category->slug = "bmw";
             $category->save();
         });
+        // Kopiraj fotku
+        $this->copyImage("bmw.jpeg");
         // Kreiraj vozilo u BMW kategoriji
         factory(App\Vehicle::class, 1)->create()->each(function($vehicle) use ($bmw){
             $vehicle->name = "BMW X5";
             $vehicle->slug = "bmw-x5";
             $vehicle->category_id = $bmw->first()->id;
+            $vehicle->image = "storage/uploads/".date("Y/m")."/bmw.jpeg";
             $vehicle->save();
         });
 
@@ -57,12 +60,48 @@ class DatabaseSeeder extends Seeder
             $category->slug = "volkswagen";
             $category->save();
         });
+        // Kopiraj fotku
+        $this->copyImage("vw.jpeg");
         // Kreiraj vozilo u VW kategoriji
         factory(App\Vehicle::class, 1)->create()->each(function($vehicle) use ($vw){
             $vehicle->name = "Volkswagen Touareg";
             $vehicle->slug = "volkswagen-touareg";
             $vehicle->category_id = $vw->first()->id;
+            $vehicle->image = "storage/uploads/".date("Y/m")."/vw.jpeg";
+            $vehicle->save();
+        });
+
+        // Kreiraj Renault kategoriju
+        $renault = factory(App\Category::class, 1)->create()->each(function($category){
+            $category->name = "Renault";
+            $category->slug = "renault";
+            $category->save();
+        });
+        // Kopiraj fotku
+        $this->copyImage("renault.jpeg");
+        // Kreiraj vozilo u Renault kategoriji
+        factory(App\Vehicle::class, 1)->create()->each(function($vehicle) use ($renault){
+            $vehicle->name = "Renault 5";
+            $vehicle->slug = "renault-5";
+            $vehicle->year = 1987;
+            $vehicle->category_id = $renault->first()->id;
+            $vehicle->image = "storage/uploads/".date("Y/m")."/renault.jpeg";
             $vehicle->save();
         });
     }
+
+    public function copyImage($image){
+        // Lokacija fajlova
+        $savePath   = "uploads/".date("Y/m");
+
+        // Kreiraj direktorij ako ne postoji
+        if( !Storage::exists("public/".$savePath) ){
+            Storage::makeDirectory("public/".$savePath);
+        }
+
+        if( !Storage::exists("public/".$savePath."/".$image) ){
+            Storage::copy($image, "public/".$savePath."/".$image);
+        }
+    }
+    
 }
